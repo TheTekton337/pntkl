62983e0c143bd9ce8fae459cf5de1d38041a4b19
Terrance Wood <terrance.james.wood@gmail.com>
2024-03-08 03:35:28 -0800

    Disabled jest testing due to mocking hell.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|react-navigation-tabs' +
+      '|react-native-splash-screen' +
+      '|react-native-screens' +
+      '|react-native-reanimated' +
+      '|react-native-webview' +
+      '|react-native-paper' +
+      '|@react-native(-community)' +
+      ')/)',
+  ],
 };
